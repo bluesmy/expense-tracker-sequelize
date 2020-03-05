7b15b4de4bdb951b397f1ec2b21ed69c91f549c4
Miao Yueh Su <bluesmy1020@gmail.com>
2020-03-05 13:45:41 +0800

    Add route and install express-handlebars

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
   res.send('hello world')
+})
+
+app.get('/record', (req, res) => {
+  res.send('列出所有支出清單與總金額')
+})
+
+app.get('/record/new', (req, res) => {
+  res.send('新增支出頁面')
+})
+
+app.post('/record', (req, res) => {
+  res.send('新增支出')
+})
+
+app.get('/record/:id/edit', (req, res) => {
+  res.send('修改支出頁面')
+})
+
+app.post('/record/:id/edit', (req, res) => {
+  res.send('修改支出')
+})
+
+app.post('record/:id/delete', (req, res) => {
+  res.send('刪除支出')
 })
 
 app.listen(3000, (req, res) => {
